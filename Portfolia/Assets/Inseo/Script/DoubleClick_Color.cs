0158b00e22a3cf8629f8403c8e1ef75138132186
Sarisol0510 <70375141+Sarisol0510@users.noreply.github.com>
2022-09-30 01:12:56 +0900

    Color Box
    안녕하세요 ㅎㅎ

---
+++ Portfolia/Assets/Inseo/Script/DoubleClick_Color.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleClick_Color : MonoBehaviour
+{
+    float interval = 0.25f;
+    float doubleClickedTime = -1.0f;
+    bool pickerOnOff = false;
+
+    private void OnMouseUp()
+    {
+        if ((Time.time - doubleClickedTime) < interval)
+        {
+            doubleClickedTime = -1.0f;
+
+            if (CircleColorPicker.Instance.gameObject.activeSelf == false)
+            {
+                pickerOnOff = true;
+                CircleColorPicker.Instance.gameObject.SetActive(true);
+
+                CircleColorPicker.Instance.linkedObject = this.gameObject;
+            }
+            else
+            {
+                /* 현재 연결된 오브젝트가 아닌 경우는 끌 수 없음 */
+                if (CircleColorPicker.Instance.linkedObject.name != this.gameObject.name) return;
+
+                pickerOnOff = false;
+                CircleColorPicker.Instance.gameObject.SetActive(false);
+                CircleColorPicker.Instance.linkedObject = null;
+            }
+        }
+        else
+        {
+            doubleClickedTime = Time.time;
+        }
+    }
+
+    private void Update()
+    {
+        if (pickerOnOff)
+        {
+            CircleColorPicker.Instance.gameObject.transform.position
+                = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * 2);
+        }
+    }
+}
