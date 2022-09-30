69dbaf87800bc56e9690141a620765bb86cac825
SYiee <zyx2009@naver.com>
2022-09-30 20:08:01 +0900

    MainScene Character 추가
    Main Camera 변경
    캐릭터 움직임 + 애니메이션

---
+++ Portfolia/Assets/SoYeon/Emotion.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emotion : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Greet()
+    {
+
+    }
+}
